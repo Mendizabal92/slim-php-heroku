3258ccd964dd57c46d4ba9762484573f924e33ea
Mendizabal92 <jmendi.92@hotmail.com>
2021-04-13 20:44:34 -0300

    13/04

---
+++ ejercicio20/registro.php
@@ -0,0 +1,21 @@
+<?php
+include "usuario.php";
+
+$miUsuario = new Usuario();
+$miUsuario->nombre=$_POST["nombre"];
+$miUsuario->clave=$_POST["clave"];
+$miUsuario->mail=$_POST["mail"];
+//Usuario::Alta($miUsuario);
+//var_dump($miUsuario);
+
+if (Usuario::ValidarUsuario($miUsuario) == "OK") {
+    //var_dump($miUsuario);
+    Usuario::Alta($miUsuario);
+    echo "se guardo correctamente";
+}
+else{
+    echo "no se guardo";
+}
+
+
+?>
